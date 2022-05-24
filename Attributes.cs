ee569ce9add05b662576f464c0011901b403b093
Mikhail Novikau <melmenx@gmail.com>
2022-05-24 16:18:39 +0300

    Create and add Attribute into branch

---
+++ Attributes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    public sealed class CheckAgeAttribute : Attribute
+    {
+        public int Age { get; set; }
+        public CheckAgeAttribute(int age)
+        {
+            Age = age;
+        }
+        public CheckAgeAttribute() { }
+    }
+   
+    public sealed class CheckLengthLoginPasswordAttribute : Attribute
+    {
+        public int Length { get; set; }
+        public CheckLengthLoginPasswordAttribute(int length)
+        {
+            Length = length;
+        }
+        public CheckLengthLoginPasswordAttribute() { }
+    }
+    
+    
+  
+}
